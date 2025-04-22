70b0abefe324dd9ef3b5557e7b42e7c5d5f400fc
Vivekanand Pujar <vivekanand.pujar@icloud.com>
2025-04-22 12:07:26 +0530

    useRef implementation

--- src/Sample.tsx
+++ src/Sample.tsx
@@ -1,46 +1,19 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import * as React from 'react';
 
-const Sample = () => {
-  const [index, setIndex] = useState(0);
-  const [conversionFactor, setConversionFactor] = useState(1);
+interface SampleProps {}
 
-  const increment = () => {
-    console.log('increment is executing');
-    setIndex((v) => v + 1);
-  };
+const Sample: React.FunctionComponent<SampleProps> = (props: SampleProps) => {
+  const inputRef = React.useRef<HTMLInputElement>(null); //  Step 1:
 
-  useEffect(() => {
-    console.log('useEffect');
-    setConversionFactor((v) => {
-      if (index < 10) {
-        return 1;
-      } else {
-        return 2;
-      }
-    });
-  }, [index]);
-
-  const expensiveFunction = (i) => {
-    console.log('expensiveFunction is executing');
-    return i * i * i;
-  };
-
-  const memoizedValue = useMemo(
-    () => expensiveFunction(conversionFactor),
-    [conversionFactor]
-  );
-
-  //  If it is not memoized, then the expensiveFunction keeps on executing...
-  //  const nonMemoizedValue = expensiveFunction(conversionFactor);
+  //  Step 3: Using the ref, execute the browser API
+  React.useEffect(() => {
+    inputRef?.current?.focus();
+  }, []);
 
   return (
     <>
-      <h3>
-        Sample Component: Index: {index}; Cube: {memoizedValue}
-      </h3>
-      <hr />
-
-      <button onClick={increment}>Increment</button>
+      {/* Step 2: Assign ref to the element */}
+      <input type='text' className='form-control' ref={inputRef} />
     </>
   );
 };
